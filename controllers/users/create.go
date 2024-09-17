935f9266f29d73e7d5f92ad2ef0d936b0f4ff035
Yo.Pitchayakorn <98311957+arteyo14@users.noreply.github.com>
2024-09-18 01:20:41 +0700

    เพิ่ม logic convert password เป็น hash

--- controllers/users/create.go
+++ controllers/users/create.go
@@ -30,12 +30,19 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
+	hashedPassword, err := response.HashPassword(user.Password)
+	if err != nil {
+		response.JSONErrorResponse(c, http.StatusInternalServerError, err)
+		return
+	}
+
+	user.Password = hashedPassword
 	user.CreateAt = time.Now()
 	user.UpdateAt = time.Now()
 
 	query := "INSERT INTO users (username, password, first_name, last_name, email, birthdate, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id"
 
-	err := config.DB.QueryRow(context.Background(), query, user.Username, user.Password, user.Firstname, user.LastName, user.Email, user.Birthdate, user.CreateAt, user.UpdateAt).Scan(&user.ID)
+	err = config.DB.QueryRow(context.Background(), query, user.Username, user.Password, user.Firstname, user.LastName, user.Email, user.Birthdate, user.CreateAt, user.UpdateAt).Scan(&user.ID)
 	if err != nil {
 		response.JSONErrorResponse(c, http.StatusInternalServerError, err)
 		return
